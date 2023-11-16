2b97c2687231ca7bfaf00461ef49107fbcdd8618
Svetajism <sv.zacharova@gmail.com>
2023-11-16 21:39:01 +0100

    Initial Commit

---
+++ NC1AppleClock/NC1AppleClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  NC1AppleClockApp.swift
+//  NC1AppleClock
+//
+//  Created by Svetlana Zakharova on 16/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NC1AppleClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
